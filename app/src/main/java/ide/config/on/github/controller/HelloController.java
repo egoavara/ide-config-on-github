16c5e2820b5066ce6139c893f528b6a0d187e03c
kane.woz(김성현)/kakao <kane.woz@kakaocorp.com>
2023-04-26 10:42:30 +0900

    feat: git을 통한 IDE 세팅 관리

---
+++ app/src/main/java/ide/config/on/github/controller/HelloController.java
@@ -0,0 +1,17 @@
+package ide.config.on.github.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Value("${idetest.whoami}")
+    private String whoami;
+
+    @GetMapping("/")
+    public String index() {
+        return String.format("Hello world, i am %s", whoami);
+    }
+
+}
